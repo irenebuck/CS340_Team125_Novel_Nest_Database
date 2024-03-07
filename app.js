5cfa416df1b274eda1070aa4f0a3195800dc0153
Irene Buck <irenedevito@gmail.com>
2024-03-06 21:54:12 -0800

    Step5
    Added .hbs files, intersection tables are not populating, CRUD implementation pending

--- app.js
+++ app.js
@@ -4,37 +4,120 @@
 // Code source: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%207%20-%20Dynamically%20Deleting%20Data
 
 
-//    SETUP for simple web app
 
-// Express
+var db = require('./database/db-connector')
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var express = require('express');   // We are using the express library for the web server
 var app = express();            // We need to instantiate an express object to interact with the server in our code
+
+PORT = 27291;                 // Set a port number at the top so it's easy to change in the future
+
+app.engine('.hbs', engine({ extname: ".hbs" }));
+app.set('view engine', '.hbs');
 app.use(express.json())         // lines 7-9 enable express to handle JSON and form data
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
-PORT = 27291;                 // Set a port number at the top so it's easy to change in the future
 
-// Handlebars
-const { query } = require('express-handlebars');
-var exphbs = require('express-handlebars');
-app.engine('.hbs', exphbs.engine({ extname: ".hbs", defaultLayout: "main" }));
-app.set('view engine', '.hbs');
 
-// Database
-var db = require('./database/db-connector')
+// Data Manipulation Queries
+// --select authors(add all author data in addition to new "book(s)" column)
+// SELECT
+// Authors.first_name AS "First Name",
+//     Authors.last_name AS "Last Name",
+//         GROUP_CONCAT(Books.title ORDER BY Books.title ASC SEPARATOR ', ') AS Books
+// FROM Authors
+// LEFT JOIN Book_has_Authors ON Authors.author_id = Book_has_Authors.author
+// LEFT JOIN Books ON Book_has_Authors.book = Books.book_id
+// GROUP BY Authors.author_id, Authors.first_name;
+
+// --select sales(add all sales data in addition to new "book(s) sold" column and replace all fks with actual names)
+// SELECT
+// Stores.store_name as "Location",
+//     Customers.name as "Customer",
+//     Sales.sales_no_tax as "Sale (No Tax)",
+//     Sales.tax_collected as "Tax Collected",
+//     Sales.purchase_date as "Purchase Date",
+//     GROUP_CONCAT(Books.title ORDER BY Books.title ASC SEPARATOR ', ') AS Books
+// FROM Sales
+// LEFT JOIN Stores ON Sales.location = Stores.store_id
+// LEFT JOIN Customers ON Sales.sale_customer = Customers.customer_id
+// LEFT JOIN Sales_has_Books ON Sales.sale_id = Sales_has_Books.sale
+// LEFT JOIN Books ON Sales_has_Books.book = Books.book_id
+// GROUP BY
+// Stores.store_name,
+//     Customers.name,
+//     Sales.sales_no_tax,
+//     Sales.tax_collected,
+//     Sales.purchase_date;
+
+// --- INSERTS-- -
+
+//     --insert a new store
+// INSERT INTO Stores(store_name, store_address)
+// VALUES(: store_name, : store_address);
+
+// --insert a new customer
+// INSERT INTO Customers(name, email)
+// VALUES(: name, : email);
+
+// --insert a new author
+// INSERT INTO Authors(first_name, last_name)
+// VALUES(: first_name, : last_name);
+
+// --insert a new book(this insert will require a query getting a store_id)
+// INSERT INTO Books(location, title, genre, purchased_price)
+// VALUES(: location, : title, : genre, : purchased_price);
+
+// --insert a new sale(this insert will require a query getting store_id, customer_id)
+// INSERT INTO SALES(location, sale_customer, sales_no_tax, tax_collected, purchase_date)
+// VALUES(: location, : sale_customer, : sales_no_tax, : tax_collected, : purchase_date);
+
+// --- DELETES-- -
+
+//     -- delete a book from Books(this delete will require a query getting a book_id)
+// DELETE FROM Books
+// WHERE book_id = : book_id;
+
+// --- UPDATES-- -
+
+//     --update the customer associated with a sale
+// UPDATE Sales
+// SET sale_customer = : sale_customer
+// WHERE sale_id = : sale_id;
+
+// --- DYNAMIC SEARCH-- -
+
+//     --search for sales by customer
+// SELECT
+// Stores.store_name as "Location",
+//     Customers.name as "Customer",
+//     Sales.sales_no_tax as "Sale (No Tax)",
+//     Sales.tax_collected as "Tax Collected",
+//     Sales.purchase_date as "Purchase Date",
+//     GROUP_CONCAT(Books.title ORDER BY Books.title ASC SEPARATOR ', ') AS Books
+// FROM Sales
+// LEFT JOIN Stores ON Sales.location = Stores.store_id
+// LEFT JOIN Customers ON Sales.sale_customer = Customers.customer_id
+// LEFT JOIN Sales_has_Books ON Sales.sale_id = Sales_has_Books.sale
+// LEFT JOIN Books ON Sales_has_Books.book = Books.book_id
+// WHERE Customers.name = : Customers.name
+// GROUP BY
+// Stores.store_name,
+//     Customers.name,
+//     Sales.sales_no_tax,
+//     Sales.tax_collected,
+//     Sales.purchase_date;
 
 /*
     ROUTES
 */
 
-
-// GET ROUTES
-
 // renders the Home page
 app.get('/', function (req, res) {
     res.render('index');
 });
-
 
 // renders the Books page
 app.get('/books', function (req, res) {
@@ -43,9 +126,8 @@ app.get('/books', function (req, res) {
     let query1;
 
     if (req.query.title === undefined) {
-        query1 = "SELECT * FROM Books;";
+        query1 = `SELECT * FROM Books`;
     }
-
     // If there is a query string, we assume this is a search, and return desired results
     else {
         query1 = `SELECT * FROM Books WHERE title LIKE "${req.query.title}%"`
@@ -64,39 +146,57 @@ app.get('/books', function (req, res) {
             // Save the stores
             let stores = rows;
 
-            return res.render('index', { data: books, stores: stores });
+            return res.render('books', { data: books, stores: stores });
         })
     })
 });
 
-
 // renders the Authors page
 app.get('/authors', function (req, res) {
-    let query1 = "SELECT * FROM Authors;";
+    let query1 = `SELECT * FROM Authors;`;
     db.pool.query(query1, function (error, rows, fields) {
-        res.render('index', { data: rows });
+        res.render('authors', { data: rows });
     })
 });
 
-
+// renders the Books_has_Authors page
 app.get('/book_has_authors', function (req, res) {
-    res.render('book_has_authors')
-})
+    let query1 = `SELECT * FROM Book_has_Authors;`;
+    db.pool.query(query1, function (error, rows, fields) {
+        res.render('book_has_authors', { data: rows });
+    })
+});
 
+// renders the Stores page
 app.get('/stores', function (req, res) {
-    res.render('stores')
-})
+    let query1 = `SELECT * FROM Stores;`;
+    db.pool.query(query1, function (error, rows, fields) {
+        res.render('stores', { data: rows });
+    })
+});
 
+// renders the Customers page
 app.get('/customers', function (req, res) {
-    res.render('customers')
-})
+    let query1 = `SELECT * FROM Customers;`;
+    db.pool.query(query1, function (error, rows, fields) {
+        res.render('customers', { data: rows });
+    })
+});
 
+// renders the Sales page
 app.get('/sales', function (req, res) {
-    res.render('sales')
-})
+    let query1 = `SELECT * FROM Sales;`;
+    db.pool.query(query1, function (error, rows, fields) {
+        res.render('sales', { data: rows });
+    })
+});
 
+// renders the Sales_has_Books page
 app.get('/sales_has_books', function (req, res) {
-    res.render('sales_has_books')
+    let query1 = `SELECT * FROM Sales_has_Books;`;
+    db.pool.query(query1, function (error, rows, fields) {
+        res.render('sales_', { data: rows });
+    })
 })
 
 

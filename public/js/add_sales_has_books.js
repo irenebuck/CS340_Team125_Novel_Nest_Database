f94bfe1ef1d4e0435367cedbc2e2fdfceabdca56
Irene Buck <irenedevito@gmail.com>
2024-03-18 12:38:19 -0700

    Merge corrections

--- public/js/add_sales_has_books.js
+++ public/js/add_sales_has_books.js
@@ -7,101 +7,92 @@
 
 // Get the objects we need to modify
 let addSalesHasBooksForm = document.getElementById(
-  "add-sales-has-books-form-ajax"
+    "add-sales-has-books-form-ajax"
 );
 
 // Modify the objects we need
 addSalesHasBooksForm.addEventListener("submit", function (e) {
-  //   debugger;
-  console.log("adding sales has books -");
+    // Prevent the form from submitting
+    e.preventDefault();
 
-  // Prevent the form from submitting
-  e.preventDefault();
+    // Get form fields we need to get data from
+    let inputSale = document.getElementById("input-sale");
+    let inputBook = document.getElementById("input-book");
 
-  // Get form fields we need to get data from
-  let inputSale = document.getElementById("input-sale");
-  let inputBook = document.getElementById("input-book");
+    // Get the values from the form fields
+    let BookValue = inputBook.value;
+    let SaleValue = inputSale.value;
 
-  // Get the values from the form fields
-  let BookValue = inputBook.value;
-  let SaleValue = inputSale.value;
+    // Put our data we want to send in a javascript object
+    let data = {
+        sale: SaleValue,
+        book: BookValue,
+    };
 
-  // Put our data we want to send in a javascript object
-  let data = {
-    sale: SaleValue,
-    book: BookValue,
-  };
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-sales-has-books-form-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
 
-  console.log(data);
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        // console.log("xhttp block");
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
 
-  // Setup our AJAX request
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "/add-sales-has-books-form-ajax", true);
-  xhttp.setRequestHeader("Content-type", "application/json");
+            // Clear the input fields for another transaction
+            inputSale.value = "";
+            inputBook.value = "";
+        } else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log(
+                "There was an error with the input. status: ",
+                xhttp.status,
+                " readystate: ",
+                xhttp.readyState
+            );
+        }
+    };
 
-  // Tell our AJAX request how to resolve
-  xhttp.onreadystatechange = () => {
-    // console.log("xhttp block");
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      // Add the new data to the table
-      addRowToTable(xhttp.response);
-
-      // Clear the input fields for another transaction
-      inputSale.value = "";
-      inputBook.value = "";
-    } else if (xhttp.readyState == 4 && xhttp.status != 200) {
-      console.log(
-        "There was an error with the input. status: ",
-        xhttp.status,
-        " readystate: ",
-        xhttp.readyState
-      );
-    }
-  };
-
-  // Send the request and wait for the response
-  xhttp.send(JSON.stringify(data));
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
 });
 
 addRowToTable = (data) => {
-  // Get a reference to the current table on the pPrice and clear it out.
-  let currentTable = document.getElementById("sale_books_table");
+    // Get a reference to the current table on the pPrice and clear it out.
+    let currentTable = document.getElementById("sale_books_table");
 
-  // Get a reference to the new row from the database query (last object)
-  let parsedData = JSON.parse(data);
-  let newRow = parsedData[parsedData.length - 1];
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1];
 
-  // Create a row and 4 cells
-  let row = document.createElement("TR");
-  let idCell = document.createElement("TD");
-  let SaleCell = document.createElement("TD");
-  let BookCell = document.createElement("TD");
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let SaleCell = document.createElement("TD");
+    let BookCell = document.createElement("TD");
+    deleteCell = document.createElement("button");
+    deleteCell.innerHTML = "Delete";
 
-  deleteCell = document.createElement("button");
-  deleteCell.innerHTML = "Delete";
+    deleteCell.onclick = function () {
+        deleteSalesHasBooks(parsedData[parsedData.length - 1].sales_has_books_id);
+    };
 
-  console.log(
-    "id where new delete is going: ",
-    parsedData[parsedData.length - 1].sales_has_books_id
-  );
-  deleteCell.onclick = function () {
-    deleteSalesHasBooks(parsedData[parsedData.length - 1].sales_has_books_id);
-  };
+    // Fill the cells with correct data
+    idCell.innerText = newRow.sales_has_books_id;
+    SaleCell.innerText = newRow.sale;
+    BookCell.innerText = newRow.title;
 
-  // Fill the cells with correct data
-  idCell.innerText = newRow.sales_has_books_id;
-  SaleCell.innerText = newRow.sale;
-  BookCell.innerText = newRow.title;
+    // Add the cells to the row
+    row.appendChild(idCell);
+    row.appendChild(SaleCell);
+    row.appendChild(BookCell);
+    row.appendChild(deleteCell);
 
-  // Add the cells to the row
-  row.appendChild(idCell);
-  row.appendChild(SaleCell);
-  row.appendChild(BookCell);
-  row.appendChild(deleteCell);
+    // Add a row attribute so the deleteRow function can find a newly added row
+    row.setAttribute("data-value", newRow.sales_has_books_id);
 
-  // Add a row attribute so the deleteRow function can find a newly added row
-  row.setAttribute("data-value", newRow.sales_has_books_id);
-
-  // Add the row to the table
-  currentTable.appendChild(row);
+    // Add the row to the table
+    currentTable.appendChild(row);
 };
+
